e2c6dd913512ab78239a0f2ec85f5b18d37bcc22
guilhermeinstacarro <guilherme.castro@instacarro.com>
2020-04-19 20:38:39 -0300

    Add new features to users Because:     users need make update in email or password

--- product/product.py
+++ product/product.py
@@ -28,3 +28,5 @@ class RegisterProduct(Connections):
 
 class RegisterStock(Connections):
     ...
+
+
--- users/users.py
+++ users/users.py
@@ -1,9 +1,9 @@
 import uuid
+from http import HTTPStatus
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from connections import Connections
-from http import HTTPStatus
 
 
 class Users(Connections):
@@ -12,12 +12,13 @@ class Users(Connections):
         self.email = email
         self.password = password
         self.register_data = register_data
+        self.data_user = self.get_user()
 
-    def _get_user(self):
+    def get_user(self):
         return self.table.select(self.table.c.email == self.email).execute().first()
 
     def register(self):
-        if self._get_user():
+        if self.data_user:
             return f'The Customer already exist in the database: {self.email}', HTTPStatus.CONFLICT
         else:
             hash_password = generate_password_hash(self.password)
@@ -32,41 +33,52 @@ class Users(Connections):
             return "successful registration", HTTPStatus.CREATED
 
     def login(self):
-        get_user = self._get_user()
-        if get_user:
-            check_password = check_password_hash(get_user['password'], self.password)
-            return ('data is correct', HTTPStatus.OK) if check_password else ('data is not correct',
-                                                                              HTTPStatus.BAD_REQUEST)
+        if self.data_user:
+            check_password = check_password_hash(self.data_user.password, self.password)
+            return ({'text': 'data is correct', '_id': self.data_user.id}, HTTPStatus.OK) if check_password \
+                else ('data is not correct', HTTPStatus.BAD_REQUEST)
         else:
             return F'{self.email}  is not found', HTTPStatus.NOT_FOUND
 
-    @property
-    def forgot_password(self):
-        return self.password
 
-    @forgot_password.setter
-    def forgot_password(self, new_password):
-        password_modification = generate_password_hash(new_password)
+class UpdateUsers(Users):
+    def change_password(self, new_password):
+        new_password = generate_password_hash(new_password)
+        with self.connect() as conn:
+            conn.excute(
+                self.table.update().where(self.table.c.id == self.data_user.id).values(password=new_password)
+            )
+        return "Change with success", HTTPStatus.NO_CONTENT
+
+    def update_data_of_register(self, new_data):
         with self.connect() as conn:
             conn.execute(
-                self.table.update().where(self.table.c.email == self.email).values(password=password_modification)
+                self.table.update().where(self.table.c.id == self.data_user.id).values(registration_form=new_data)
             )
-        self.password = new_password
+        data = self.get_user()
+        return {'message': "Change with success", 'data': data}, HTTPStatus.NO_CONTENT
+
+    def update_email(self, new_email):
+        with self.connect() as conn:
+            conn.execute(
+                self.table.update().where(self.table.c.id == self.data_user.id).values(email=new_email)
+            )
+        self.email = new_email
+        data = self.get_user()
+        return {'message': "Change with success", 'data': data}, HTTPStatus.NO_CONTENT
 
 
-class Provider(Users):
+class ConfirmationUser(Connections):
+    def __init__(self, table, user_id):
+        super().__init__(table_name=table)
+        self.id = user_id
 
-    def edit_register_data(self, new_data):
-        ...
+    def confirmation(self):
+        with self.connect() as conn:
+            conn.execute(
+                self.table.update().where(self.table.c.id == self.id).values(active=True)
+            )
+        return 'Confirmation with success', HTTPStatus.NO_CONTENT
 
-    def deleted(self):
-        ...
 
 
-class Client(Users):
-
-    def edit_register_data(self, new_data):
-        ...
-
-    def deleted(self):
-        ...
